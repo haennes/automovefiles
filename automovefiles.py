d3344b73676eeef644468e6fc9be32f029c94ddd
haennes <hannes.hofmuth@gmail.com>
2021-04-03 14:18:28 +0200

    moved out the some parts of make config commit befor moving out all_classes

--- automovefiles.py
+++ automovefiles.py
@@ -19,16 +19,46 @@ def load_config():
     f.close()
     return to_return
 
+def make_config_path(message):
+    while True:
+        path = input(message)
+        if os.path.isdir(path):
+            return path
+
+def make_config_case():
+    ignore_upper_and_lower_case = bool(input("should upper and lower case be ignored (1 Yes, 0 No)"))
+    return ignore_upper_and_lower_case
+
+def make_config_classes():
+    all_classes_entered = False
+    all_classes = []
+    while not all_classes_entered:
+        current_class_name = input("type the folder-name of the class (Example: English) (if you are done press enter)").strip()
+        all_classes_entered = not (current_class_name and current_class_name.strip())
+        if not all_classes_entered:
+            all_classes.append(make_config_classes_aliases(current_class_name))
+    return all_classes
+
+def make_config_classes_aliases(current_class_name):
+    all_aliases_for_class_entered = False
+    all_aliases_for_class = []
+    print("next you can enter aliases for the subject")
+    while not all_aliases_for_class_entered:
+        current_alias_for_current_class = input("Please enter another alias for the subject (if you are done press enter)").strip()
+        all_aliases_for_class_entered = not (current_alias_for_current_class and current_alias_for_current_class.strip())
+        if not all_aliases_for_class_entered:
+            all_aliases_for_class.append(current_alias_for_current_class)
+            print("entered aliases for ",current_class_name,":" , all_aliases_for_class)
+        else: print("all aliases for ",current_class_name,"entered :",all_aliases_for_class)
+    return all_aliases_for_class
+
 def make_config():
-    while True:
-        config_source = input("please enter the folder that should be scanned")
-        if os.path.isdir(config_source):
-            break
-    while True:
-        config_dest = input("please enter the folder the files should be moved to (parent of class names)")
-        if os.path.isdir(config_dest):
-            break
-    config_ignore_upper_and_lower_case = bool(input("should upper and lower case be ignored (1 Yes, 0 No)"))
+    config_source = make_config_path("please enter the folder that should be scanned")
+
+    config_dest = make_config_path("please enter the folder the files should be moved to (parent of class names)")
+
+    config_ignore_upper_and_lower_case = make_config_case()
+
     all_classes_entered = False
     all_classes = []
     while not all_classes_entered:
@@ -55,10 +85,24 @@ def make_config():
 
     save_config(all_classes,config_source,config_dest,config_ignore_upper_and_lower_case)
 
-
-
+def change_config():
+    current_config = load_config()
+    current_classes , current_source, current_dest, current_ignore_upper_and_lower_case = current_config
+    print("current config:")
+    print(current_config)
+    change_what = input("What do you want to change: classes , source, dest, ignore upper and lower case")
+    if  change_what == "classes":
+        pass
+    elif change_what == "source":
+        pass
+    elif change_what == "dest":
+        pass
+    elif change_what == "ingore upper and lower case" or change_what == "ingore case":
+        pass
 
 classes, source, dest, ignore_upper_and_lower_case = load_config()
+if bool(input("would you like to change the current config ? (1 Yes, 0 No)").strip()):
+    change_config()
 print(load_config())
 files = os.listdir(source)
 print(files)
