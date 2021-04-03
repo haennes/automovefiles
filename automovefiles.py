385c52da1c939b90139128781a803f5a671954d4
haennes <hannes.hofmuth@gmail.com>
2021-04-03 18:30:52 +0200

    make everything changable

--- automovefiles.py
+++ automovefiles.py
@@ -70,18 +70,35 @@ def change_config():
     current_classes , current_source, current_dest, current_ignore_upper_and_lower_case = current_config
     print("current config:")
     print(current_config)
-    change_what = input("What do you want to change: classes , source, dest, ignore upper and lower case")
-    if  change_what == "classes":
-        pass
-    elif change_what == "source":
-        pass
-    elif change_what == "dest":
-        pass
-    elif change_what == "ingore upper and lower case" or change_what == "ingore case":
-        pass
+    change_what = input("What do you want to change: classes , source, dest, ignore_upper_and_lower_case  (seperate multiple with spaces)").split()
+    
+    if  "classes" in change_what:
+        print("current classes",current_classes)
+        classes_to_change_indexes = input("what class(es) do you want to change ? (seperate them by a space) (enter the indexes (1 = first))").split()
+        for i in classes_to_change_indexes:
+            aliases_to_change_indexes = input("what aliases do you want to change ? (seperate them by a space) (0 for Foldername)").split()
+            for ii in aliases_to_change_indexes:
+                new_alias = input("Enter the name you want "+ ii +"to change to. (space to remove)").split()[0]
+                if new_alias == "":
+                    current_classes[i].pop(ii)
+                else:
+                    current_classes[int(i)][int(ii)] = new_alias
+
+
+    if "source" in change_what:
+        current_source = make_config_path("please enter the folder that should be scanned")
+
+    if "dest" in change_what:
+        current_dest = make_config_path("please enter the folder the files should be moved to (parent of class names)")
+
+    if "ignore_upper_and_lower_case" in change_what or "ignore_case" in change_what:
+        current_ignore_upper_and_lower_case = make_config_case()
+
+    save_config(current_classes,current_source,current_dest,current_ignore_upper_and_lower_case)
 
 classes, source, dest, ignore_upper_and_lower_case = load_config()
-if bool(input("would you like to change the current config ? (1 Yes, 0 No)").strip()):
+
+if bool(int(input("would you like to change the current config ? (1 Yes, 0 No)"))):
     change_config()
 print(load_config())
 files = os.listdir(source)
